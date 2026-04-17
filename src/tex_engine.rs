0a42c26e0f1eb6acd4ddcf2a54a45c81002c7080
rice8y <eitoy1128@mbr.nifty.com>
2026-04-17 15:52:14 +0900

    feat(tex): add TeX engine option parsing

---
+++ src/tex_engine.rs
@@ -0,0 +1,25 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum TexEngine {
+    Auto,
+    PdfTeX,
+    LuaTeX,
+    XeTeX,
+    PTeX,
+    UpTeX,
+}
+
+impl std::str::FromStr for TexEngine {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "auto" => Ok(TexEngine::Auto),
+            "pdftex" => Ok(TexEngine::PdfTeX),
+            "luatex" => Ok(TexEngine::LuaTeX),
+            "xetex" => Ok(TexEngine::XeTeX),
+            "ptex" => Ok(TexEngine::PTeX),
+            "uptex" => Ok(TexEngine::UpTeX),
+            _ => Err(format!("Unknown engine: {}", s)),
+        }
+    }
+}
